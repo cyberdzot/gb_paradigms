62a680581403887be74c5a0e4c945d6bfd6e0869
Vovqa <vovan102vb@gmail.com>
2023-09-09 22:23:53 +0300

    added fix for hw01

--- Homework01/main.py
+++ Homework01/main.py
@@ -1,17 +1,15 @@
 # Дан список целых чисел numbers. Необходимо написать в императивном и декларативном стилях процедуры для
 # сортировки числа в списке в порядке убывания. Можно использовать любой алгоритм сортировки.
 
-def sort_list_imperative(numbers):
-    isRepeat = True
+def sort_list_imperative(list):
+    is_repeat = True
 
-    while (isRepeat):
-        isRepeat = False
-        for i in range(0, len(numbers) - 1):
-            if numbers[i] > numbers[i + 1]:
-                temp = numbers[i]
-                numbers[i] = numbers[i + 1]
-                numbers[i + 1] = temp
-                isRepeat = True
+    while is_repeat:
+        is_repeat = False
+        for i in range(0, len(list) - 1):
+            if list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
+                is_repeat = True
 
 
 def sort_list_declarative(numbers):

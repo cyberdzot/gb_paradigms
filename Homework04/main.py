30b0f65c2643e2012c5081814cdd6992bb7ccbe2
Vovqa <vovan102vb@gmail.com>
2023-09-16 18:56:33 +0300

    Added HW04

---
+++ Homework04/main.py
@@ -0,0 +1,15 @@
+from numpy import corrcoef as corr_pirson
+from random import randint as rand_int
+
+
+def get_corr(arr1: list[int], arr2: list[int]) -> int:
+    return corr_pirson(arr1, arr2)[0, 1]
+
+
+if __name__ == '__main__':
+    array1 = [rand_int(0, 9) for _ in range(10)]
+    array2 = [rand_int(0, 9) for _ in range(10)]
+
+    print(array1)
+    print(array2)
+    print(f'Корреляция Пирсона между массивами: {get_corr(array1, array2)}')
